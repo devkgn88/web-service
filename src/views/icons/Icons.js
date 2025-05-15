af53d5b7573b562335747b38e9d97f8af0ee693d
devkgn88 <devkgn88@gmail.com>
2025-05-15 10:48:04 +0900

    [김가남] 메뉴에 사용 예정인 아이콘 추출

--- src/views/icons/Icons.js
+++ src/views/icons/Icons.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PageContainer from 'src/components/container/PageContainer';
 import DashboardCard from '../../components/shared/DashboardCard';
-
+import {
+  IconBuilding,
+  IconCalendar,
+  IconMessage,
+  IconLayoutDashboard
+} from '@tabler/icons-react';
 
 const Icons = () => {
   return (
     <PageContainer title="Icons" description="this is Icons">
 
       <DashboardCard title="Icons">
-      <iframe src="https://tabler-icons.io/"  title="Inline Frame Example" frameBorder={0}
-    width="100%"
-    height="650"></iframe>
+    <div style={{ display: 'flex', gap: '20px' }}>
+      <div><IconBuilding size={32} /> <p>회의실 목록</p></div>
+      <div><IconCalendar size={32} /> <p>예약 현황</p></div>
+      <div><IconMessage size={32} /> <p>문의</p></div>
+      <div><IconLayoutDashboard size={32} /> <p>대시보드</p></div>
+    </div>
       </DashboardCard>
     </PageContainer>
   );
